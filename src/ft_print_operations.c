a01bab407b5813879f6c60a8961ca8a19beadc44
cghael <55437891+cghael@users.noreply.github.com>
2020-04-19 14:03:48 +0300

    ft_print_operations created

--- src/ft_print_operations.c
+++ src/ft_print_operations.c
@@ -6,5 +6,13 @@
 
 void		ft_print_operations(t_order *cmd_stack)
 {
+	t_order	*tmp;
 
+	tmp = cmd_stack;
+	while (tmp != NULL)
+	{
+		ft_putstr(tmp->command);
+		ft_putchar('\n');
+		tmp = tmp->next;
+	}
 }
